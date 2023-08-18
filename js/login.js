16795775004048768b9411fd7fb6e101cd7139dd
susmoy15 <107901530+susmoy15@users.noreply.github.com>
2023-08-18 10:15:40 +0600

    baap er bank done

--- js/login.js
+++ js/login.js
@@ -1,4 +1,14 @@
-console.log(thik thak);
 document.getElementById('btn-submit').addEventListener('click',function(){
-    console.log(okay);
+     const emailField = document.getElementById('user-email');
+     const email = emailField.value;
+     console.log(email);
+     const passwordField = document.getElementById('user-password');
+     const password = passwordField.value;
+    console.log(password);
+    if(email === 'susmoy@gmail.com' && password === 'bless'){
+        window.location.href = 'bank.html';
+    }
+    else{
+        alert("Invalid user")
+    }
 })
---
+++ js/withdraw.js
@@ -0,0 +1,25 @@
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    // console.log('okay');
+
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    // console.log(newWithdrawAmount);
+
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+
+    withdrawField.value = '';
+})
